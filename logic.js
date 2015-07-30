a112d82a73deeeb52466b3511cbeed1832a63cff
BenKai3 <benjaminerkan@gmail.com>
2015-07-30 00:39:02 -0700

    Still working on the parens, but it's time for bed. Maybe I'll pull a double helix and find the answer in a dream :). All unfinished code is commented to allow for proper functioning.

--- logic.js
+++ logic.js
@@ -1,9 +1,41 @@
+var equation = "";
+var answers = [];
+var almost_answer = '';
+var parens_count;
+// var first_parens = '';
+// var second_parens = '';
+// var third_parens = '';
+
 window.onload = function () {
-	var equation = "";
-	var answers = [];
 	var answer_screen = document.getElementById('screen');
 	var numbers = document.getElementsByClassName('number');
 	var operators = document.getElementsByClassName('operator');
+// RUNNING THE EQUATION
+	document.getElementById('equals').onclick = function(){
+		if (/*equation.search('(') == -1 && */eval(equation) != undefined) {
+			answers.push(eval(equation));
+		}
+		// else {
+		// 	parens_count = equation.replace(/[^(]/g, "").length;
+		// 	for (var i = 0; i < parens_count; i++) {
+		// 		parens_count[i];
+		// 	};
+		// 	for (var i = 0; i < equation.length; i++) {
+		// 		if (equation[i-1] == '(') {
+		// 			1st_parens += equation[i]
+		// 		};
+		// 		if (equation[i+1] == ')') {
+
+		// 		}
+		// 		else if (1st_parens != '') {
+
+		// 		};
+		// 	};
+		// };
+		console.log('answers: '+answers);
+		answer_screen.innerHTML = answers[answers.length-1];
+		equation = "";
+	};
 // CLEARING THE EQUATION/SCREEN/ANSWER HISTORY
 	document.getElementById('clear_last').onclick = function(){
 		answers.pop();
@@ -18,15 +50,6 @@ window.onload = function () {
 		answers.pop();
 		equation = "";
 		console.log('answers: '+answers);
-	};
-// RUNNING THE EQUATION
-	document.getElementById('equals').onclick = function(){
-		if (eval(equation) != undefined) {
-			answers.push(eval('equation: '+equation));
-		};
-		console.log('answers: '+answers);
-		answer_screen.innerHTML = answers[answers.length-1];
-		equation = "";
 	};
 //BUILDING THE EQUATION
 	for (var i = 0; i < numbers.length; i++) {
